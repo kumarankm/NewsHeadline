a2ba160eb01ce1a737d5d79caab4dc9eb6f4e901
Kumaran KM <kumaranmohan1106@gmail.com>
2021-09-08 11:36:34 +0530

    Add files via upload

---
+++ headbuild.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome(".\chromedriver_win32\chromedriver.exe")
+driver.get(r'C:\Users\Admin\Desktop\Headline detector\buildd\index.html')
+
+i = 1
+arr = []
+for _ in range(3):
+    ca = "head" + str(i)
+    arr.append(driver.find_element_by_id(ca).text)
+    i += 1
+print(arr)
